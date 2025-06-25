48634246963bcd003b9e7b98acd41dc6a97cb814
webDevSociapa <webdev@sociapa.com>
2025-06-25 12:55:47 +0530

    Triggering re-deployment

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,6 +10,8 @@ const geistSans = localFont({
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
