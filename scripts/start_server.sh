a7dbf55a06283db5b266aecb59abdd58d075979b
webDevSociapa <webdev@sociapa.com>
2025-06-25 14:01:36 +0530

    fixesss

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/ec2-user/SociapaDashboard
-pm2 stop sociapa-dashboard || true
-pm2 start npm --name "sociapa-dashboard" -- start
+pm2 stop SociapaDashboard || true
+pm2 start npm --name "SociapaDashboard" -- start
 pm2 save
